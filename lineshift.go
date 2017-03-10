d72692a0fd6620185f59bb030851f95f6684319b
chinatsu <lolexplode@gmail.com>
2017-03-10 10:17:43 +0100

    Rename program in help

--- lineshift.go
+++ lineshift.go
@@ -11,6 +11,6 @@ func main() {
   if len(args) == 1 {
     fmt.Printf("%+q\n", fileutils.GetLineending(args[0])) // Safely print newline characters
   } else {
-    fmt.Println("Usage: printfile [filename]")
+    fmt.Println("Usage: lineshift [filename]")
   }
 }
